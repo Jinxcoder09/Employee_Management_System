c258be3437ad6aa779ab8d7f62942e5141743c37
MANISH KUMAR <164140756+Jinxcoder09@users.noreply.github.com>
2025-06-20 01:25:31 +0530

    Update settings.py

--- employee_project/settings.py
+++ employee_project/settings.py
@@ -106,4 +106,4 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-CORS_ALLOWED_ORIGINS = [ "https://greentree.up.railway.app/",]
+CORS_ALLOWED_ORIGINS = True
